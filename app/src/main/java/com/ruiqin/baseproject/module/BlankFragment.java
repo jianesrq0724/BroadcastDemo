e384f75f4b16d8cbcdad31cc78a40017f8727f89
shenruiqin <ruiqin.shen@100cb.cn>
2017-08-10 09:41:07 +0800

    初始化

---
+++ app/src/main/java/com/ruiqin/baseproject/module/BlankFragment.java
@@ -0,0 +1,29 @@
+package com.ruiqin.baseproject.module;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ruiqin.baseproject.R;
+import com.ruiqin.baseproject.base.BaseFragment;
+
+public class BlankFragment extends BaseFragment {
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void stopLoad() {
+
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+        
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_blank;
+    }
+}
